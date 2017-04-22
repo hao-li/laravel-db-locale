f9b90efef5f669e3405ada504522e3c3b73858c4
hao li <haoli.sand@gmail.com>
2017-04-22 19:16:26 +0800

    Initial commit

---
+++ src/Traits/DbLocaleTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HaoLi\LaravelDbLocale\Traits;
+
+trait DbLocaleTrait
+{
+    public function getMutatedAttributes()
+    {
+        $attributes = parent::getMutatedAttributes();
+        return array_merge($attributes, $this->getDbLocaleFields());
+    }
+
+    protected function mutateAttributeForArray($key, $value)
+    {
+        return (in_array($key, $this->getDbLocaleFields()))
+            ? $this->getDbLocaleColumnValue($key)
+            : parent::mutateAttributeForArray($key, $value);
+    }
+
+    public function getAttributeValue($key)
+    {
+        $value = parent::getAttributeValue($key);
+        if (in_array($key, $this->getDbLocaleFields())) {
+            $this->getDbLocaleColumnValue($key);
+        }
+
+        return $value;
+    }
+
+    public function getDbLocaleFields()
+    {
+        return (property_exists($this, 'dbLocaleFields')) ? $this->dbLocaleFields : [];
+    }
+
+    private static function getDbLocaleColumnName($key, $locale)
+    {
+        return $key . '_' . str_replace('-', '_', strtolower($locale));
+    }
+
+    private function getDbLocaleColumnValue($key)
+    {
+        $defaultColumn = self::getDbLocaleColumnName($key, config('app.locale'));
+        $fallbackColumn = self::getDbLocaleColumnName($key, config('app.fallback_locale'));
+        if (array_key_exists($defaultColumn, $this->attributes)) {
+            $ret = $this->attributes[$defaultColumn];
+        } else if (array_key_exists($fallbackColumn, $this->attributes)) {
+            $ret = $this->attributes[$fallbackColumn];
+        } else {
+            $ret = null;
+        }
+        return $ret;
+    }
+}
